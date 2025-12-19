c41d6d7435cc9a0c1049edda42765b0931b11086
TwelveStudent47 <laczkokevin60@gmail.com>
2025-12-19 15:53:43 +0100

    Created the product model.

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const fs = require("fs")
+const path = require("path")
+const pathMod = require("../util/path")
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title
+    }
+
+    save() {
+        const p = path.join(pathMod, "data", "products.json")
+        fs.readFile(p, (err, data) => {
+            let products = []
+            if (!err) {
+                products = JSON.parse(data.toString())
+            }
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
+    }
+
+    static fetchAll(cb) {
+        const p = path.join(pathMod, "data", "products.json")
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([])
+            }
+            cb(JSON.parse(fileContent.toString()))
+        })
+    }
+}
